9af5e2e537fffd06493836452427ef4e3c5d765a
Eusth <chikakarayattekita@yahoo.co.jp>
2016-05-26 21:45:59 +0200

    Improve IK/FK handling.

--- PlayClubStudioVR/GripHandler.cs
+++ PlayClubStudioVR/GripHandler.cs
@@ -15,6 +15,7 @@ namespace PlayClubStudioVR
     {
         private readonly static int GIZMO_LAYER = LayerMask.NameToLayer("DriveUI");
         private Controller _Controller;
+        private bool _Dragging = false;
         private RumbleSession _Rumble;
         private Controller.Lock _Lock;
         private Transform _Target;
@@ -22,8 +23,10 @@ namespace PlayClubStudioVR
 
         private Vector3 _PrevPos;
         private Quaternion _PrevRot;
-        private Quaternion _StartRot;
         private GuideDriveManager _Manager;
+        private bool _Hover;
+        private GameObject _HoverObject;
+
 
 
         protected override void OnStart()
@@ -51,12 +54,12 @@ namespace PlayClubStudioVR
                 var device = GetInput();
                 if(device.GetPressDown(EVRButtonId.k_EButton_SteamVR_Trigger)) {
                     _PrevPos = transform.position;
-                    _StartRot = _Manager.rotRoot.localRotation;
                     _PrevRot = transform.rotation;
+                    _Rumble.MilliInterval = 500;
+                    _Dragging = true;
                 }
                 if (device.GetPress(EVRButtonId.k_EButton_SteamVR_Trigger))
                 {
-
                     _Manager.DriveMovePosition(transform.position - _PrevPos);
 
                     var rotDiff = (transform.rotation) * Quaternion.Inverse(_PrevRot);
@@ -64,6 +67,19 @@ namespace PlayClubStudioVR
                     
                     _PrevPos = transform.position;
                     _PrevRot = transform.rotation;
+                }
+                if(device.GetPressUp(EVRButtonId.k_EButton_SteamVR_Trigger))
+                {
+                    _Dragging = false;
+
+                    if (_Hover)
+                    {
+                        _Rumble.MilliInterval = 100;
+                    }
+                    else
+                    {
+                        Release();
+                    }
                 }
             }
         }
@@ -77,7 +93,7 @@ namespace PlayClubStudioVR
         {
             if(other.gameObject.layer == GIZMO_LAYER && HasFocus())
             {
-                try
+                if (!_Dragging)
                 {
                     if (_Rumble != null)
                     {
@@ -86,23 +102,45 @@ namespace PlayClubStudioVR
                     _Rumble = new RumbleSession(500, 100);
                     _Controller.StartRumble(_Rumble);
                     _Manager = (_GuideDriveManager.GetValue(other.GetComponent<GuideDrive>()) as GuideDriveManager);
-                    Logger.Info("Started rumbling");
-
-                } catch(Exception e)
+                    _HoverObject = other.gameObject;
+                    _Hover = true;
+                } else
                 {
-                    Logger.Error(e);
+                    if(other.gameObject == _HoverObject)
+                    {
+                        _Hover = true;
+                    }
                 }
+                
             }
         }
 
         public void OnTriggerExit(Collider other)
         {
-            if(other.gameObject.layer == GIZMO_LAYER && HasFocus())
+            if (other.gameObject.layer == GIZMO_LAYER && HasFocus())
             {
-                Logger.Info("Ended rumbling");
+                if (!_Dragging)
+                {
+                    Release();
+                } else
+                {
+                    if (other.gameObject == _HoverObject)
+                    {
+                        _Hover = false;
+                    }
+                }
+            }
+        }
 
+        private void Release()
+        {
+            if (_Rumble != null)
+            {
                 _Rumble.Close();
                 _Rumble = null;
+            }
+            if (HasFocus(false))
+            {
                 _Lock.Release();
             }
         }
